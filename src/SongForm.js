a7b1c9617f5318c50959accf54af76d321afa575
Andrew Owens <drew2447@gmail.com>
2022-06-01 14:28:35 -0600

    first

--- src/SongForm.js
+++ src/SongForm.js
@@ -3,17 +3,35 @@ import { Form } from 'semantic-ui-react'
 
 
 class SongForm extends React.Component {
-  state = {}
+  state = {
+    song: "",
+    band: "",
+  }
 
-  handleChange = (e, { value }) => this.setState({ value })
-
+  //handleChange = (e, { value }) => this.setState({ value })
+  handleSubmit = (e) => {
+    console.log('click')
+    this.props.addSong(this.state)
+    this.setState({
+      song: "",
+      band: "",
+    })
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.song]: e.target.value
+    })
+  }
   render() {
   
     return (
-      <Form>
+      <Form onSubmit={this.handleSubmit}>
         <Form.Group widths='equal'>
-          <Form.Input fluid label='Song' placeholder='song' />
-          <Form.Input fluid label='Band' placeholder='band' />
+          <Form.Input fluid label='Song' 
+          placeholder='song'
+          value={this.state.song} 
+          onChange={this.handleChange}/>
+          <Form.Input fluid label='Band' placeholder='band' value={this.state.band} onChange={this.handleChange}/>
           <Form.Button>Submit</Form.Button>
         </Form.Group>
       </Form>
